92f2b28abb449210e23c76850789901e225f651c
sklj <david.krizov@gmail.com>
2023-03-06 08:44:22 +0100

    funkcija dodata

--- blink/test.c/zad1.c
+++ blink/test.c/zad1.c
@@ -9,17 +9,10 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <stdint.h>
-int16_t main ()
+void ledBlink (int16_t high_time , int16_t low_time)
 {
-	int16_t high_time = 300;
-	int16_t low_time = 700;
-	DDRB |= 1 << 5; // PB5 je izlaz
-	while (1)
-	{
-		PORTB |= 1 << 5; // LED ON
-		_delay_ms (high_time) ; // Pauza 1 s
-		PORTB &= ~(1 << 5) ; // LED OFF
-		_delay_ms (low_time) ; // Pauza 1 s
-	}
-		return 0;
+	PORTB |= 1 << 5; // LED ON
+	_delay_ms (high_time) ; // Pauza 1 s
+	PORTB &= ~(1 << 5) ; // LED OFF
+	_delay_ms (low_time) ; // Pauza 1 s
 }
