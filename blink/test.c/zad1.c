19306a170baf426bbb10167a8db075eea124b59d
sklj <david.krizov@gmail.com>
2023-03-06 08:28:07 +0100

    Inital commit

---
+++ blink/test.c/zad1.c
@@ -0,0 +1,25 @@
+/*
+ * zad1.c
+ *
+ *  Created on: Mar 6, 2023
+ *      Author: student
+ */
+
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdint.h>
+int16_t main ()
+{
+	int16_t high_time = 500;
+	int16_t low_time = 500;
+	DDRB |= 1 << 5; // PB5 je izlaz
+	while (1)
+	{
+		PORTB |= 1 << 5; // LED ON
+		_delay_ms (high_time) ; // Pauza 1 s
+		PORTB &= ~(1 << 5) ; // LED OFF
+		_delay_ms (low_time) ; // Pauza 1 s
+	}
+		return 0;
+}
